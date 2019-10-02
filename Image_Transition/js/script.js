246a5ff8ff5f3e79a0bb18992014d4cdeb93e06e
Kumar Sourav <souravjsr2011@gmail.com>
2019-10-02 23:10:37 +0530

    added Image_Transition

---
+++ Image_Transition/js/script.js
@@ -0,0 +1,26 @@
+new hoverEffect({
+    parent:document.querySelector('.image-content1'),
+    intensity: 0.2,
+    image1:'../images/pexels-photo-1366919.jpeg',
+    image2:'../images/pexels-photo-1624496.jpeg',
+    displacementImage:'../images/heightMap.png'
+});
+
+
+// new hoverEffect({
+//     parent:document.querySelector('.image-content2'),
+//     intensity: 0.2,
+//     image1:'../images/black-and-white-data-electronics-234871.jpg',
+//     image2:'../images/electronics-flatlay-notebook-1211752.jpg',
+//     // speedIn:2,
+//     // speedOut:3,
+//     displacementImage:'../images/stripe1.png'
+// });
+
+// new hoverEffect({
+//     parent:document.querySelector('.image-content3'),
+//     intensity: 0.6,
+//     image1:'../images/jared.jpg',
+//     image2:'../images/joker.png',
+//     displacementImage:'../images/haha.png'
+// });
