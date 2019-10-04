5d2855a8ac68553b89ba10eceb5425f60cd38ea6
Kumar Sourav <souravjsr2011@gmail.com>
2019-10-05 02:55:45 +0530

    changed intensity

--- Joker Image Transition/js/script.js
+++ Joker Image Transition/js/script.js
@@ -1,6 +1,6 @@
 new hoverEffect({
     parent:document.querySelector('.image'),
-    intensity: 0.6,
+    intensity: 0.2,
     image1:'heath.jpeg',
     image2:'joaquin.jpeg',
     displacementImage:'heightMap.png',
